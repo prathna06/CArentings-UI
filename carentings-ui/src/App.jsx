b2f3d737cf3927e8caf2c90fadd07836f5d350ca
Prathana Gunawat <Prathna.gunawat@equipped.ai>
2023-07-01 11:59:22 -0700

    app bar

---
+++ carentings-ui/src/App.jsx
@@ -0,0 +1,12 @@
+  import React from 'react';
+import AppBar from './Components/AppBar'
+  const App = () => {
+    return(
+        <div>
+        {/*   <h1>hey</h1> */}
+            <AppBar/>
+        </div>
+    )
+  }
+
+  export default App;   
