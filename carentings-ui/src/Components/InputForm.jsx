e51d6fdae46c66c8936be52aa9408e2b4b67dba8
Prathana Gunawat <Prathna.gunawat@equipped.ai>
2023-07-14 19:22:18 -0700

    merge conflict

--- carentings-ui/src/Components/InputForm.jsx
+++ carentings-ui/src/Components/InputForm.jsx
@@ -1,52 +1,49 @@
-import * as React from "react";
-import { useState } from "react";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { Button, FormControl, TextField, Autocomplete } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import axios from "axios";
-import { fetchOptionsRequest } from "../Utils/actions/actions";
-import { useSelector, useDispatch } from "react-redux";
-import { makeStyles } from "@mui/styles";
-import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+import { useState } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Button, FormControl, TextField, Autocomplete } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import { fetchOptionsRequest } from '../Utils/actions/actions';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@mui/styles';
+import { useNavigate } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   formControl: {
-    position: "absolute",
+    position: 'absolute',
+  },
+  zipCodeField: {
+    marginTop: '16px',
+  },
+  categoryField: {
+    marginTop: '16px',
   },
 }));
 
 const categories = [
-  { title: "Cornhole" },
-  { title: "Ladder Toss" },
-  { title: "Jenga" },
-  { title: "Tag" },
-  { title: "spikeball" },
+  { title: 'Cornhole' },
+  { title: 'Ladder Toss' },
+  { title: 'Jenga' },
+  { title: 'Tag' },
+  { title: 'Spikeball' },
 ];
 
 function InputForm() {
-  const [eventDate, setEventDate] = useState(dayjs().add(0, "day"));
-  const [Zipcode, setZipcode] = useState("");
+  const [eventDate, setEventDate] = useState(dayjs().add(0, 'day'));
+  const [Zipcode, setZipcode] = useState('');
   const [Category, setCategory] = useState([]);
   const [eventDateError, setEventDateError] = useState(false);
   const [ZipcodeError, setZipcodeError] = useState(false);
-  // const [CategoryError, setCategoryError] = useState(false);
-  const navigate = useNavigate();
   const [options, setOptions] = useState([]);
-  // const options = useSelector((state) => state.options.options);
-  //  const loading = useSelector((state) => state.options.loading);
-  //  const dispatch = useDispatch();
-  /*     React.useEffect(() => {
-        dispatch(fetchOptionsRequest());
-      }, [dispatch]); */
+
   const handleInputChange = (event) => {
     const keyword = event.target.value;
     axios
-      .get(
-        `https://carenting-api.azurewebsites.net/api/category/lookUp?keyword=${keyword}`
-      )
+      .get(`https://carenting-api.azurewebsites.net/api/category/lookUp?keyword=${keyword}`)
       .then((response) => {
         setOptions(response.data);
       })
@@ -54,14 +51,14 @@ function InputForm() {
         console.error(error);
       });
   };
+
   const handleEventDateChange = (date) => {
     setEventDateError(false);
     setEventDate(date);
   };
 
   const validateZipcode = () => {
-    // debugger;
-    if (Zipcode.trim() === "") {
+    if (Zipcode.trim() === '') {
       setZipcodeError(true);
     } else if (!/^\d{5}(?:[-\s]\d{4})?$/.test(Zipcode)) {
       setZipcodeError(true);
@@ -71,95 +68,93 @@ function InputForm() {
     }
   };
 
-    const handleZipcodeChange = (e) => {
-        setZipcodeError(false);
-        setZipcode(e.target.value);
-    
-    };
- 
-    const handleCategoryChange = (e, value) => {
-        // setCategoryError(false);
-        setCategory(value);
-    };
+  const handleZipcodeChange = (e) => {
+    setZipcodeError(false);
+    setZipcode(e.target.value);
+    axios({
+      url: `https://carenting-api.azurewebsites.net/api/location/lookUp?keyword=${e.target.value}`,
+      method: 'GET',
+      headers: {
+        authorization: 'your token comes here',
+      },
+    })
+      .then((res) => {})
+      .catch((err) => {});
+  };
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
+  const handleCategoryChange = (e, value) => {
+    setCategory(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
     if (!Zipcode) {
       setZipcodeError(true);
       return;
     } else if (validateZipcode()) {
-      console.log("eventDate:", eventDate.$d);
-      console.log("Zipcode:", Zipcode);
-      console.log("Category:", Category);
+      console.log('eventDate:', eventDate.$d);
+      console.log('Zipcode:', Zipcode);
+      console.log('Category:', Category);
     }
   };
 
-    const classes = useStyles();
-    
-  return (
-    <FormControl component='form' onSubmit={handleSubmit} className={classes.formControl}>     
-        <LocalizationProvider dateAdapter={AdapterDayjs} >
-       
-            <DatePicker
-                label="Event Date"
-                value={eventDate}
-                defaultValue={dayjs().add(0, 'day')}
-                onChange={handleEventDateChange}
-                disablePast
-                error={eventDateError}
-            />
-        </LocalizationProvider>
+  const navigate = useNavigate();
+  const classes = useStyles();
 
-      {/* {eventDateError && !eventDate && (
-          <Typography variant="body2" color="error" style={{ marginBottom: '1rem' }}>
-            Event Date is required
-          </Typography>
+  const handleButtonClick = () => {
+    navigate('/shoppingPage');
+  };
+
+  return (
+    <FormControl component="form" onSubmit={handleSubmit} className={classes.formControl}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Event Date"
+          value={eventDate}
+          defaultValue={dayjs().add(0, 'day')}
+          onChange={handleEventDateChange}
+          disablePast
+          error={eventDateError}
+        />
+      </LocalizationProvider>
+
+      <TextField
+        label="Zipcode"
+        value={Zipcode}
+        onChange={handleZipcodeChange}
+        id="filled-hidden-label-Normal"
+        style={{marginTop:"1rem"}}
+        variant="outlined"
+        size="Normal"
+        autoComplete="off"
+        className={classes.zipCodeField}
+        error={ZipcodeError}
+      />
+
+      <Autocomplete
+        options={options}
+        getOptionLabel={(option) => option.label}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            style={{marginTop:"1rem"}}
+            variant="outlined"
+            onChange={handleInputChange}
+            className={classes.categoryField}
+          />
         )}
-        <TextField
-            label="Zipcode"
-            value={Zipcode}
-            onChange={handleZipcodeChange}
-            id="filled-hidden-label-Normal"
-            variant="outlined"
-            size="Normal"
-            autoComplete="off"
-            style={{ marginTop: '1rem' }}
-            error={ZipcodeError}
-        />
-        {/* <TextField
-            label="Category"
-            id="filled-hidden-label-normal"
-            value={Category}
-            onChange={handleCategoryChange}
-            variant="outlined"
-            autoComplete="off"
-            style={{ marginTop: '1rem' }}
-            error={CategoryError}
-        /> */}
-        <Autocomplete
-            multiple
-            id="tags-outlined"
-            options={categories}
-            value={Category}
-            getOptionLabel={(option) => option.title}
-            onChange={handleCategoryChange}
-            // defaultValue={[Category]}
-            filterSelectedOptions
-            style={{ marginTop: '1rem' }}
-            renderInput={(params) => (
-            <TextField
-                {...params}
-                label="Items"
-                placeholder="Categories"
-            />
-            )}
-        />
-        <Button variant="contained" color="inherit" type="submit"  
-            style={{ marginTop: '1rem',background:"#1C76D2", color:"white" }}>
-            Submit
-        </Button>
-       
+      />
+      <Button
+        variant="contained"
+        color="inherit"
+        type="submit"
+        onClick={handleButtonClick}
+        style={{ marginTop: '16px', background: '#1C76D2', color: 'white' }}
+      >
+        Submit
+      </Button>
     </FormControl>
   );
 }
